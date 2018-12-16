7e967544aaab7c66818329a945881a693148f249
Bart Whiteley <@bwhiteley>
2018-12-15 21:00:28 -0700

    Filtering support for ItemsViewController with UISearchController

--- Sources/ItemsViewController.swift
+++ Sources/ItemsViewController.swift
@@ -3,7 +3,7 @@
 //  SwiftBits
 //
 //  Created by Bart Whiteley on 11/27/16.
-//  Copyright © 2016 SwiftBit. All rights reserved.
+//  Copyright © 2018 SwiftBit. All rights reserved.
 //
 
 // https://talk.objc.io/episodes/S01E26-generic-table-view-controllers-part-2
@@ -26,16 +26,52 @@ public struct CellDescriptor {
     }
 }
 
-open class ItemsViewController<Item>: UITableViewController {
-    public var items: [[Item]] = []
+open class ItemsViewController<Item>: UITableViewController, UISearchResultsUpdating {
+    public var items: [[Item]] = [] {
+        didSet {
+            filteredItems = items
+        }
+    }
+    var filteredItems: [[Item]] = []
     let cellDescriptor: (Item) -> CellDescriptor
     public var didSelect: (Item) -> () = { _ in }
     var reuseIdentifiers: Set<String> = []
+    
+    // This makes it behave like Mail.app. Setting this to true will
+    // override navigationItem.hidesSearchBarWhenScrolling
+    public var showsSearchBarOnFirstLoadThenHidesWhenScrolling: Bool = false
+    
+    public let searchController: UISearchController = UISearchController(searchResultsController: nil)
+    
+    // If you supply this closure it will add a search bar to the top of the table
+    // and apply the provided filter function when filtering.
+    public var searchFilter: ((String, Item) -> Bool)? {
+        didSet {
+            if searchFilter == nil {
+                if #available(iOS 11.0, *) {
+                    self.navigationItem.searchController = nil
+                } else {
+                    self.tableView.tableHeaderView = nil
+                }
+            }
+            else {
+                if #available(iOS 11.0, *) {
+                    self.navigationItem.searchController = self.searchController
+                } else {
+                    self.tableView.tableHeaderView = self.searchController.searchBar
+                }
+            }
+        }
+    }
     
     public init(items: [[Item]], style: UITableViewStyle = .plain, cellDescriptor: @escaping (Item) -> CellDescriptor) {
         self.cellDescriptor = cellDescriptor
         super.init(style: style)
         self.items = items
+        self.filteredItems = items
+        self.searchController.dimsBackgroundDuringPresentation = false
+        self.searchController.searchResultsUpdater = self
+        self.definesPresentationContext = true // get rid of warning: The topViewController of the navigation controller containing the presented search controller must have definesPresentationContext set to YES. Also, if we don't do this the nav bar disappears when we pop :face_with_rolling_eyes:
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -43,20 +79,21 @@ open class ItemsViewController<Item>: UITableViewController {
     }
     
     override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = items[indexPath.section][indexPath.row]
-        didSelect(item)
+        let item = filteredItems[indexPath.section][indexPath.row]
+        
+        self.didSelect(item)
     }
     
     override open func numberOfSections(in tableView: UITableView) -> Int {
-        return items.count
+        return filteredItems.count
     }
     
     override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items[section].count
+        return filteredItems[section].count
     }
     
     override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let item = items[indexPath.section][indexPath.row]
+        let item = filteredItems[indexPath.section][indexPath.row]
         let descriptor = cellDescriptor(item)
         
         if !reuseIdentifiers.contains(descriptor.reuseIdentifier) {
@@ -72,5 +109,35 @@ open class ItemsViewController<Item>: UITableViewController {
         descriptor.configure(cell)
         return cell
     }
-}
+    
+    open override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if #available(iOS 11.0, *) {
+            if showsSearchBarOnFirstLoadThenHidesWhenScrolling {
+                self.navigationItem.hidesSearchBarWhenScrolling = false
+            }
+        }
+    }
+    
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if #available(iOS 11.0, *) {
+            if showsSearchBarOnFirstLoadThenHidesWhenScrolling {
+                self.navigationItem.hidesSearchBarWhenScrolling = true
+            }
+        }
+    }
 
+    public func updateSearchResults(for searchController: UISearchController) {
+        guard let filterFunc = self.searchFilter else { return }
+        guard let searchText = searchController.searchBar.text, !searchText.isEmpty else {
+            filteredItems = items
+            tableView.reloadData()
+            return
+        }
+        filteredItems = items.map { (subItems:[Item]) in
+            return subItems.filter { filterFunc(searchText, $0) }
+        }
+        tableView.reloadData()
+    }
+}
